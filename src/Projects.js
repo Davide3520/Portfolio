f7e8de9b8e2e3249f1993bd9b30d59f281b0893f
Davide Lepri <davidelepri3@gmail.com>
2021-12-21 17:27:17 -0500

    Skills added

--- src/Projects.js
+++ src/Projects.js
@@ -11,8 +11,21 @@ export const projects = [
   {
     title: "myJobs",
     subtitle: "Keep your job search organized.",
-    description: "myJobs is an app that helps you keep track of your job applications (e.g. Huntr). You can add as many jobs applications as you want, so that you can stay organized during your job search.",
+    description: "myJobs is an app that helps you keep track of your job applications (e.g. Huntr). You can add as many jobs applications as you want, so that you can stay organized during your job search. Built a REST API using Node.js, Express and Sequelize with Sequelize models. Implemented user login with password encryption and JWT authentication. Client-Side is built using React. Redux is used for state management.",
     image: "./myJobs.png",
     link: "https://my-jobs-23.herokuapp.com/login#"
   }
+]
+
+export const skills = [
+  "React",
+  "Redux",
+  "Javascript",
+  "Node.js",
+  "Sequelize",
+  "PostgreSQL",
+  "Express",
+  "CSS",
+  "HTML",
+  "Git/Github"
 ]

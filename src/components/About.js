b590737e778faba707cecd180d66259a343ef206
Davide Lepri <davidelepri3@gmail.com>
2021-12-23 13:51:25 -0500

    changes made on projects and skills components

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,8 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hello, I'm Davide and
-            <br className="hidden lg:inline-block" /> I am a Software Engineer.
+            Hi, I'm Davide and
+            <br className="hidden lg:inline-block" /> I am a Fullstack Software Engineer.
           </h1>
           <p className="mb-8 leading-relaxed">
           I was born in Cagli, a small town in central Italy, where everyone knows each other. The town is filled with history, there are still remain of ancient bridges and roads built during the Roman Empire, all while surrounded by nature. I used to spend my summer days with my friends keeping cool in the fresh water that flows from the many rivers and creeks nearby. I moved to the United States about four years ago to be with my wife, the rest of my family lives in Italy and we travel there every year to visit them. One of my biggest passions other than programming is basketball, I played for 15 years and I learned many important skills like teamwork, communication, and dependability.
